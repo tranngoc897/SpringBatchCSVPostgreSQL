7dd02c1979925624d431c69763272399fd525df1
ngoctran <tranngoc897@gmail.com>
2021-06-22 16:38:17 +0700

    Add quartz job

--- src/main/java/com/javasampleapproach/batchcsvpostgresql/SpringBatchCsvPostgreSqlApplication.java
+++ src/main/java/com/javasampleapproach/batchcsvpostgresql/SpringBatchCsvPostgreSqlApplication.java
@@ -1,9 +1,12 @@
 package com.javasampleapproach.batchcsvpostgresql;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBatchCsvPostgreSqlApplication {
 
 	public static void main(String[] args) {
